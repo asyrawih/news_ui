c3e046fee70f625528b5e193a3190fde56e131fc
Hanan Asyrawi Rivai <hanan.rivai@bitcoin.co.id>
2023-04-08 20:16:41 +0700

    add tranding news

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,38 @@
 import { Layout } from "@/components/Layout/layout";
 import { DummyCard } from "@/components/dummy/card";
-import { Container, Grid, Row, Text } from "@nextui-org/react";
+import { Col, Container, Grid, Row, Text } from "@nextui-org/react";
 
 export default function Home() {
   return (
     <Layout>
       <Headline />
+      <Videos />
     </Layout>
   )
 }
 
+const Videos = () => {
+  return (
+    <Container display="flex" justify="center" color="Black">
+      <Title title="Tranding News" />
+      <Grid.Container gap={1} justify="center" direction="row" >
+        <Grid xs={12} md={8}>
+          <DummyCard imageUrl="https://nextui.org/images/card-example-1.jpeg" />
+        </Grid>
+        <Grid xs={12} md={4}>
+          <DummyCard imageUrl="https://nextui.org/images/card-example-2.jpeg" />
+        </Grid>
+      </Grid.Container>
+    </Container>
+
+  )
+
+}
+
 const Headline = () => {
   return (
-    <Container display="flex" justify="flex-start">
-      <Title />
+    <Container display="flex" justify="flex-start" >
+      <Title title="Top News" />
       <Grid.Container gap={2} justify="center" >
         <Grid xs={12} md={4}>
           <DummyCard imageUrl="https://nextui.org/images/card-example-1.jpeg" />
@@ -35,14 +54,14 @@ const Headline = () => {
   )
 }
 
-const Title = () => {
+const Title = ({title} : {title : string}) => {
   return (
     <Container display="flex" justify="space-around" css={{
       marginTop: "2rem"
     }}>
       <Row justify="space-between" align="flex-end">
         <Text h2>
-          Top News
+          {title}
         </Text>
         <Text>
           See More
