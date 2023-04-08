bf49be17001462f90e479b26d18098b36b91c05b
Hanan Asyrawi Rivai <hanan.rivai@bitcoin.co.id>
2023-04-08 21:39:04 +0700

    add real dummy news

--- src/components/dummy/card.tsx
+++ src/components/dummy/card.tsx
@@ -2,19 +2,21 @@ import { Card, Col, Text } from "@nextui-org/react"
 import React from "react"
 
 type DummyProps = {
+  title: string
   imageUrl: string
+  desc: string
 }
 
-export const DummyCard: React.FC<DummyProps> = ({ imageUrl }) => {
+export const DummyCard: React.FC<DummyProps> = ({ imageUrl, title, desc }) => {
   return (
     <Card>
       <Card.Header css={{ position: "absolute", zIndex: 1, top: 5 }}>
         <Col>
-          <Text size={12} weight="bold" transform="uppercase" color="#ffffffAA">
-            What to watch
-          </Text>
           <Text h4 color="white">
-            Stream the Acme event
+            {title}
+          </Text>
+          <Text size={12} weight="bold" transform="uppercase" color="#ffffffAA">
+            {desc}
           </Text>
         </Col>
       </Card.Header>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,27 +1,52 @@
 import { Layout } from "@/components/Layout/layout";
 import { DummyCard } from "@/components/dummy/card";
-import { Col, Container, Grid, Row, Text } from "@nextui-org/react";
+import { Container, Grid, Row, Text } from "@nextui-org/react";
+import { GetServerSideProps } from "next";
+import React from "react";
 
-export default function Home() {
+export type News = {
+  status: string
+  totalResults: number
+  articles: Article[]
+}
+
+export type Article = {
+  source: Source
+  author: string
+  title: string
+  description: string
+  url: string
+  urlToImage: string
+  publishedAt: string
+  content: string
+}
+
+export type Source = {
+  id: string
+  name: string
+}
+
+export default function Home({ data }: { data: News }) {
   return (
     <Layout>
-      <Headline />
-      <Videos />
+      <Headline articles={data.articles} />
+      <TrandingNews articles={data.articles} />
     </Layout>
   )
 }
 
-const Videos = () => {
+const TrandingNews = ({ articles }: Pick<News, "articles">) => {
   return (
     <Container display="flex" justify="center" color="Black">
       <Title title="Tranding News" />
       <Grid.Container gap={1} justify="center" direction="row" >
-        <Grid xs={12} md={8}>
-          <DummyCard imageUrl="https://nextui.org/images/card-example-1.jpeg" />
-        </Grid>
-        <Grid xs={12} md={4}>
-          <DummyCard imageUrl="https://nextui.org/images/card-example-2.jpeg" />
-        </Grid>
+        {articles.slice(6, 24).map((item, index) => {
+          return (
+            <Grid xs={12} md={4} key={index}>
+              <DummyCard title={item.title} imageUrl={item.urlToImage} desc={item.description} />
+            </Grid>
+          )
+        })}
       </Grid.Container>
     </Container>
 
@@ -29,32 +54,38 @@ const Videos = () => {
 
 }
 
-const Headline = () => {
+const Headline = ({ articles }: Pick<News, "articles">) => {
   return (
     <Container display="flex" justify="flex-start" >
       <Title title="Top News" />
       <Grid.Container gap={2} justify="center" >
-        <Grid xs={12} md={4}>
-          <DummyCard imageUrl="https://nextui.org/images/card-example-1.jpeg" />
-        </Grid>
-        <Grid xs={12} md={4}>
-          <DummyCard imageUrl="https://nextui.org/images/card-example-2.jpeg" />
-        </Grid>
-        <Grid xs={12} md={4}>
-          <DummyCard imageUrl="https://nextui.org/images/card-example-3.jpeg" />
-        </Grid>
-        <Grid xs={12} md={5}>
-          <DummyCard imageUrl="https://nextui.org/images/card-example-4.jpeg" />
-        </Grid>
-        <Grid xs={12} md={7}>
-          <DummyCard imageUrl="https://nextui.org/images/card-example-5.jpeg" />
-        </Grid>
+        {articles.slice(0, 5).map((item, index) => {
+          if (index < 3) {
+            return (
+              <Grid xs={12} md={4} key={index}>
+                <DummyCard imageUrl={item.urlToImage} title={item.title} desc={item.description} />
+              </Grid>
+            )
+          }
+          if (index === 3) {
+            return (
+              <Grid xs={12} md={5} key={index}>
+                <DummyCard desc={item.description} imageUrl={item.urlToImage} title={item.title} />
+              </Grid>
+            )
+          }
+          return (
+            <Grid xs={12} md={7} key={index}>
+              <DummyCard imageUrl={item.urlToImage} title={item.title} desc={item.description} />
+            </Grid>
+          )
+        })}
       </Grid.Container>
     </Container>
   )
 }
 
-const Title = ({title} : {title : string}) => {
+const Title = ({ title }: { title: string }) => {
   return (
     <Container display="flex" justify="space-around" css={{
       marginTop: "2rem"
@@ -69,4 +100,14 @@ const Title = ({title} : {title : string}) => {
       </Row>
     </Container>
   )
+}
+
+
+export const getServerSideProps: GetServerSideProps = async (_) => {
+  const result = await fetch("https://newsapi.org/v2/everything?q=apple&from=2023-04-07&to=2023-04-07&sortBy=popularity&apiKey=de5c04e1eca847a9a80d5b62ef6b4223")
+  const data = await result.json() as News
+
+  return {
+    props: { data }
+  }
 }
