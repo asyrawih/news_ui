065735a638a23762d05dbc7f8d29fd7dae3aa8fe
Hanan Asyrawi Rivai <hanan.rivai@bitcoin.co.id>
2023-04-20 14:35:41 +0800

    add navigation back and back to top

--- src/components/StickyBar/stickybar.tsx
+++ src/components/StickyBar/stickybar.tsx
@@ -1,13 +1,18 @@
 import { Query, useMediaQuery } from "@/hooks/media-query"
-import { Container, styled } from "@nextui-org/react"
+import { Container, Spacer, Text, styled } from "@nextui-org/react"
 import { useTheme } from '@nextui-org/react'
+import { useRouter } from "next/router"
+import { ArrowLeft, ArrowUp } from "react-feather"
 
 
 export const StickyBar = () => {
   const { isDark } = useTheme()
+  const router = useRouter()
   const isMobile = useMediaQuery({
     query: Query.sm
   })
+
+
 
   const dark = "rgba(14, 13, 13, 0.5)"
   const light = "rgba(236, 236, 236, 0.5)"
@@ -20,13 +25,50 @@ export const StickyBar = () => {
     height: "80px",
     position: "fixed",
     width: "100vw",
-    bottom: "$0"
+    bottom: "$0",
   })
+
+
+  const StickyContent = styled("div", {
+    marginTop: "25px",
+    display: "flex",
+    justifyContent: "space-between"
+  })
+
+  const StickyIcon = styled("div", {
+    display: "flex",
+    flexDirection: "row",
+  })
+
+
+
+  const handleBack = () => {
+    router.back()
+  }
+
+  const handleToTop = () => {
+    window.scroll(0, 0)
+  }
+
 
   return (
     <>
       {isMobile &&
         <StickyContiner>
+          <StickyContent>
+            {router.pathname == "/posts/[read]" &&
+              <StickyIcon>
+                <ArrowLeft onClick={handleBack} />
+                <Spacer x={0.12} />
+                <Text>Back</Text>
+              </StickyIcon>
+            }
+
+            <StickyIcon>
+              <ArrowUp onClick={handleToTop} />
+              <Text>Top</Text>
+            </StickyIcon>
+          </StickyContent>
         </StickyContiner>
       }
     </>
